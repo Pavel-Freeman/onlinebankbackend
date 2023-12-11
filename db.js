d0901c8840d74fea2bfe376de7b5dc217170d58c
Pavel-Freeman <akik_2018@mail.ru>
2023-12-11 03:55:53 +0300

    fix db

--- db.js
+++ db.js
@@ -1,17 +1,30 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-module.exports = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    dialect: 'postgres',
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialectOptions: {
-      ssl: {
-        require: true
-      }
-    }
-  }
-);
+// module.exports = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     dialect: 'postgres',
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialectOptions: {
+//       ssl: {
+//         require: true
+//       }
+//     }
+//   }
+// );
+
+const { Pool } = require('pg')
+ 
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
+
+pool.connect((err) => {
+    if (err) throw err
+    console.log("Connect to PostgreSQL successfully!")
+})
+
+module.exports = pool
